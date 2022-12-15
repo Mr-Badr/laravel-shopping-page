aebf0bc4c5a3546fdc036d739307093f4c30c6d6
Badr Eddine El-Harchali <mr.elharchali@gmail.com>
2022-12-15 20:45:28 +0100

    testing routes

--- routes/web.php
+++ routes/web.php
@@ -20,3 +20,22 @@ Route::get('/', function () {
 Route::get('/about', function () {
     return view('about');
 });
+
+Route::get('/store/{category?}/{item?}', function ($category = null, $item = null) {
+    if (isset($category)) {
+        if (isset($item)){
+            return "<h1> {$item} </h1>";
+        }
+        return "<h1> {$category} </h1>";
+    }
+    return '<h1>Store</h1>';
+});
+
+/* Route::get('/store', function () {
+    
+    $filter = request('store');
+    if (isset($filter)){
+        return 'this page is viewing <span style="color: red">'.$filter.'</span>';
+    }
+    return 'this page is viewing <span style="color: red">All products</span>';
+}); */
